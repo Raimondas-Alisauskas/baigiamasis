d5f861b5e938c11fd1f76e56b6b354ebbf83c5d5
RaimondasA <ra.alisa.123@gmail.com>
2018-08-18 09:07:45 +0300

    Tarpinis

---
+++ src/com/vcs/ds/TMP/BandClientData.java
@@ -0,0 +1,39 @@
+package com.vcs.ds.TMP;
+
+import com.vcs.ds._20_client.Client;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BandClentData {
+
+    public static void main(String[] args) {
+
+
+        List<Client> clientsDatabase = new ArrayList<>();
+
+        clientsDatabase.add(new Client("N1"));
+        clientsDatabase.add(new Client("N2"));
+        clientsDatabase.add(new Client("N3"));
+
+
+    }
+}
+
+
+
+
+//    List<Client> clientsDatabase = new ArrayList<>();
+//
+//
+//
+//    Map<String, List<Mokinys>> mokiniai = new HashMap<>();
+//
+//		mokiniai.put("Jonas", sukurkJonuArr());
+//
+//    private static List<Mokinys> sukurkEgliuArr() {
+//        List<Mokinys> list = new ArrayList<>();
+
+
---
+++ src/com/vcs/ds/TMP/BandTime.java
@@ -0,0 +1,4 @@
+package com.vcs.ds.TMP;
+
+public class BandTime {
+}
