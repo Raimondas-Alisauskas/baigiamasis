13bf2557dc4b266476d46c0545ec108b5aca00ed
ModestasGi <modestas.girdzevicius@gmail.com>
2018-08-24 18:20:59 +0300

    Tarpiniai pataisymai pagal komentarą: 1. Į atskirą klasę, kaip enums'ai iškelta: 1.1. kalbų sąrašas; 1.1. pirmam paieškos etapui numatyti pasirinkimai; 2. Pirmame paieškos etape "neaiškūs skaičiai" panaikinti. Įdėti atitinkami enums'ai; 3. Pravalyti neraikalingi komentarai ir pertekliniai "system.out.println";

---
+++ src/main/java/com/vilniuscodingschool/baigiamasis/Entry.java
@@ -0,0 +1,7 @@
+package com.vilniuscodingschool.baigiamasis;
+
+public class Entry {
+
+
+
+}
---
+++ src/main/java/com/vilniuscodingschool/baigiamasis/SearchOptions.java
@@ -0,0 +1,7 @@
+package com.vilniuscodingschool.baigiamasis;
+
+public enum SearchOptions {
+
+    Autorius, Pavadinimas, ISBN, Leidykla, Kalba, Tipas
+
+}
