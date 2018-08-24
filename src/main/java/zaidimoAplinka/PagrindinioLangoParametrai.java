96e1eec6903e59253f0cbd2ca4d5ee38ffce4aed
MAC <gintare.lazauskaite@gmail.com>
2018-08-24 15:37:58 +0300

    Gui

---
+++ src/main/java/zaidimoAplinka/PagrindinioLangoParametrai.java
@@ -0,0 +1,24 @@
+package zaidimoAplinka;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PagrindinioLangoParametrai extends JPanel {
+
+
+    public PagrindinioLangoParametrai() {
+        setBorder(BorderFactory.createLineBorder(Color.yellow));
+    }
+
+        public Dimension getPreferredSize() {
+        return new Dimension(800,600);
+    }
+
+        public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Draw Text
+        g.drawString("ŽAIDIMAS",20,40);
+        
+    }
+    }
---
+++ src/main/java/zaidimoAplinka/PirmasGui.java
@@ -0,0 +1,35 @@
+package zaidimoAplinka;
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+
+public class PirmasGui extends JFrame {
+
+    private JLabel label;
+    private JButton button;
+    private JTextField textfield;
+
+    public PirmasGui() {
+        setLayout(new FlowLayout());
+
+        label = new JLabel("Žaidimas!");
+        add(label);
+
+        textfield = new JTextField(10);
+        add(textfield);
+
+        button = new JButton("CLICK ME!");
+        add(button);
+    }
+
+    public static void main(String[] args) {
+
+    PirmasGui gui = new PirmasGui();
+    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    gui.setSize(200, 200);
+    gui.setVisible(true);
+    gui.setTitle("Žaidimas");
+
+}
+
+}
